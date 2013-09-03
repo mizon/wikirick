02da7d7a574589fcac4016ecd8db65dd998a4f21
mizon <mizon9@gmail.com>
2013-09-03 17:57:54 +0900

    Add a module for importing common modules

---
+++ src/Wikirick/Import.hs
@@ -0,0 +1,11 @@
+module Wikirick.Import
+  ( module Export
+  ) where
+
+import Control.Applicative as Export
+import Control.Lens as Export
+import Control.Monad as Export
+import Control.Monad.State as Export
+import Control.Monad.Trans as Export
+import Data.Function as Export
+import Data.Monoid as Export
