eb106f1db84dfd066c860883bd6ec48c80172c80
mizon <mizon9@gmail.com>
2013-09-11 04:59:08 +0900

    Add wikirick's sub snaplets to the app

--- src/Wikirick/Application.hs
+++ src/Wikirick/Application.hs
@@ -6,17 +6,24 @@ import Snap.Snaplet.Heist
 import Snap.Snaplet.Auth
 import Snap.Snaplet.Session
 
+import Wikirick.Article
 import Wikirick.JSONConnection
+import qualified Wikirick.URLMapper as U
 
 data App = App
   { _heist :: Snaplet (Heist App)
   , _sess :: Snaplet SessionManager
   , _auth :: Snaplet (AuthManager App)
   , _json :: Snaplet JSONConnection
+  , _articles :: Snaplet (ArticleRepository App)
+  , _urlMapper :: Snaplet U.URLMapper
   }
 makeLenses ''App
 
 instance HasHeist App where
   heistLens = subSnaplet heist
+
+instance U.HasURLMapper App where
+  refURLMapper = urlMapper
 
 type AppHandler = Handler App App
