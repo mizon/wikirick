77e361cb6797e4db711d7c52754c4609dfb3b909
mizon <mizon9@gmail.com>
2013-09-19 01:59:38 +0900

    Add some examples which check to handle 'Edit' requests

--- spec/AppHandlerSpec.hs
+++ spec/AppHandlerSpec.hs
@@ -2,6 +2,7 @@ module AppHandlerSpec
   ( appHandlerSpec
   ) where
 
+import Control.Monad.CatchIO
 import qualified Data.ByteString as BS
 import qualified Data.Text as T
 import Heist
@@ -24,8 +25,8 @@ import SpecHelper
 runAppHandler :: MonadIO m => ST.RequestBuilder m () -> SnapletInit App App -> m (Either T.Text Response)
 runAppHandler req app = SST.runHandler Nothing req (route WS.routes) app
 
-shouldBeRendered :: Response' -> (BS.ByteString, Splices (I.Splice AppHandler)) -> Expectation
-shouldBeRendered res (templateName, s) = do
+beRendered :: Response' -> (BS.ByteString, Splices (I.Splice AppHandler)) -> Expectation
+beRendered res (templateName, s) = do
   res' <- mustBeRight res
   body <- ST.getResponseBody res'
   body' <- renderView
@@ -44,21 +45,61 @@ appHandlerSpec = describe "main handler" $
     it "opens article pages" $ do
       let article = def & R.articleTitle .~ "FrontPage"
       let repo' = repositoryMock
-            { R._fetchArticle = \title -> case title of
+            { R._fetchArticle = \case
                 "FrontPage" -> return article
                 _ -> fail "invalid call"
             }
       res <- runAppHandler (ST.get "/wiki/FrontPage" mempty) $ mockedApp repo'
-      res `shouldHaveStatus` 200
-      res `shouldBeRendered` ("base", V.articleSplices article)
+      res `hasStatus` 200
+      res `beRendered` ("base", V.articleSplices article)
 
     it "opens article page fragments" $ do
       let article = def
       let repo' = repositoryMock
-            { R._fetchArticle = \title -> case title of
+            { R._fetchArticle = \case
                 "FrontPage" -> return article
                 _ -> fail "invalid call"
             }
       res <- runAppHandler (xhr >> ST.get "/wiki/FrontPage" mempty) $ mockedApp repo'
-      res `shouldHaveStatus` 200
-      res `shouldBeRendered` ("article", V.articleSplices article)
+      res `hasStatus` 200
+      res `beRendered` ("article", V.articleSplices article)
+
+    it "redirects to the new article page" $ do
+      let repo' = repositoryMock
+            { R._fetchArticle = \case
+                "SomeNewPage" -> throw R.ArticleNotFound
+                _ -> fail "invalid call"
+            }
+      res <- runAppHandler (ST.get "/wiki/SomeNewPage" mempty) $ mockedApp repo'
+      res `beRedirectedTo` "/wiki/SomeNewPage/edit"
+
+    it "opens a 'New' article page" $ do
+      let repo' = repositoryMock
+            { R._fetchArticle = \case
+                "SomeNewPage" -> throw R.ArticleNotFound
+                _ -> fail "invalid call"
+            }
+
+      res <- runAppHandler (ST.get "/wiki/SomeNewPage/edit" mempty) $ mockedApp repo'
+      res `hasStatus` 200
+      res `beRendered` ("base", V.editorSplices "SomeNewPage")
+
+      res' <- runAppHandler (xhr >> ST.get "/wiki/SomeNewPage/edit" mempty) $ mockedApp repo'
+      res' `hasStatus` 200
+      res' `beRendered` ("editor", V.editorSplices "SomeNewPage")
+
+    it "opens an 'Edit' article page" $ do
+      let article = def & R.articleTitle .~ "SomePage"
+      let repo' = repositoryMock
+            { R._fetchArticle = \case
+                "SomePage" -> return article
+                _ -> fail "invalid call"
+            }
+
+      res <- runAppHandler (ST.get "/wiki/SomePage/edit" mempty) $ mockedApp repo'
+      res `hasStatus` 200
+      res `beRendered` ("base", V.editorSplices "SomePage")
+
+      res' <- runAppHandler (xhr >> ST.get "/wiki/SomePage/edit" mempty) $ mockedApp repo'
+      res' `hasStatus` 200
+      res' `beRendered` ("editor", V.editorSplices "SomePage")
